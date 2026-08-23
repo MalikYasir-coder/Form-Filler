a46c96c2428b2255e6994832793cc9e8908bce66
Malik Yasir <my4871767@gmail.com>
2026-08-23 21:42:59 +0500

    first commit

--- Form-Filler/src/Components/header.jsx
+++ Form-Filler/src/Components/header.jsx
@@ -2,7 +2,8 @@ function Header(){
     return (
         <header>
             <h1>Form Filler</h1>
-
+            <p>Just a Few Details To Get Started</p>
         </header>
     );
 }
+export default Header;
